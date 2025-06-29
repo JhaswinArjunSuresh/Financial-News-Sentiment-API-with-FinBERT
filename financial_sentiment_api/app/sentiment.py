f7b00a5ac406b0850fe55d8038903ba79a92f29e
JhaswinArjunSuresh <jhaswin.arjun.suresh@gmail.com>
2025-06-30 00:35:06 +0400

    Update sentiment.py

--- financial_sentiment_api/app/sentiment.py
+++ financial_sentiment_api/app/sentiment.py
@@ -1,1 +1,19 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from torch.nn.functional import softmax
+import torch
+
+class FinSentiment:
+    def __init__(self):
+        self.tokenizer = AutoTokenizer.from_pretrained("yiyanghkust/finbert-tone")
+        self.model = AutoModelForSequenceClassification.from_pretrained("yiyanghkust/finbert-tone")
+
+    def predict(self, text):
+        inputs = self.tokenizer(text, return_tensors="pt", truncation=True)
+        outputs = self.model(**inputs)
+        probs = softmax(outputs.logits, dim=1).detach().cpu().numpy()[0]
+        return {
+            "positive": float(probs[0]),
+            "negative": float(probs[1]),
+            "neutral": float(probs[2])
+        }
 
